96469ab0757df391ab8643591e4e879a91595024
Hikarisaotom2 <clau_cortes@unitec.edu>
2024-06-05 19:45:19 -0600

    Clase 5 de junio: create user con firebase

--- Semana_5y6/Backend/server.js
+++ Semana_5y6/Backend/server.js
@@ -1,17 +1,25 @@
 /*haciendo referencia a express*/
 const express = require('express')
-
 const  bodyParser = require('body-parser')
+
+const { initializeApp } = require('firebase/app')
+const {getAuth, createUserWithEmailAndPassword} = require('firebase/auth')
 
 const path = require('path')
 const cors = require('cors');
 const { ServerApiVersion,MongoClient, ObjectId } = require('mongodb');
 
 let port = 3001;
-
 const uri = 'mongodb+srv://usuario:password1!@ux2023.lg0z62y.mongodb.net/?retryWrites=true&w=majority&appName=ux2023'
-
-
+const firebaseConfig = {
+    apiKey: "AIzaSyBaejQ2O8MRgMtsQFFVqkMwiTx7e2_KkXA",
+    authDomain: "ux2024-6cba2.firebaseapp.com",
+    projectId: "ux2024-6cba2",
+    storageBucket: "ux2024-6cba2.appspot.com",
+    messagingSenderId: "342081729560",
+    appId: "1:342081729560:web:c891f7902249a31dd0bf6d",
+    measurementId: "G-5NG5C4NQFH"
+  };
 
 var urlEncodeParser = bodyParser.urlencoded({extended:true});
 
@@ -26,6 +34,8 @@ const client = new MongoClient(uri, {
         strict:true,
     }
 });
+
+const firebaseApp = initializeApp(firebaseConfig);
 /*
                     terminologia: 
     Endpoint: es la url a la que se le hace una peticion
@@ -58,6 +68,27 @@ app.listen(port,()=>{
 })
 
 
+app.post('/signUpFirebase',async (req,res)=>{
+    /*
+    correo, contrasena, nombre 
+    */
+   const auth = getAuth(firebaseApp);
+   const email = req.body.correo;
+   const password = req.body.contrasena;
+   try{
+         const userCredential = await createUserWithEmailAndPassword(auth,email,password);
+         res.status(200).send({
+            descripcion: 'usuario creado con exito',
+            resultado: userCredential
+        });
+   }catch(error){
+       console.error('Hubo un error al crear el usuario',error)
+       res.status(500).send({
+        descripcion: 'No se pudo crear el usuario en firebase',
+        resultado: error
+    });
+   }
+});
 
 // app.post('/saludar',(req,res)=>{})
 // app.put('/saludar',(req,res)=>{})
